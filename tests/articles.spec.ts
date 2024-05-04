8572518ab0f79a33f4c339576583b61af59c1b2b
grzesiek <grzegorz.gajownik@paypo.pl>
2024-05-04 13:24:51 +0200

    chore: organizer tests

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -1,4 +1,5 @@
 import { randomNewArticle } from '../src/factories/article.factory';
+import { AddArticleModel } from '../src/models/articles.model';
 import { ArticlePage } from '../src/pages/article.page';
 import { ArticlesPage } from '../src/pages/articles.page';
 import { LoginPage } from '../src/pages/login.page';
@@ -8,26 +9,67 @@ import { AddArticleView } from '../src/views/add-article.views';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify articlesn', () => {
-  test('create new articles @GAD_R04_01', async ({ page }) => {
-    // Arrange
-    const loginPage = new LoginPage(page);
+  let loginPage: LoginPage;
+  let articlesPage: ArticlesPage;
+  let addArticleView: AddArticleView;
+
+  let articleData: AddArticleModel
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    articlesPage = new ArticlesPage(page);
+    addArticleView = new AddArticleView(page);
+
     await loginPage.goto();
     await loginPage.loginNew(testUser1);
-
-    const articlesPage = new ArticlesPage(page);
     await articlesPage.goto();
+    await articlesPage.addArticleButtomLogged.click();
+
+    articleData = randomNewArticle();
+    await expect.soft(addArticleView.header).toBeVisible();
+  });
+
+  test('create new articles @GAD_R04_01', async ({ page }) => {
+    // Arrange
+
+    const articlePage = new ArticlePage(page);
 
     //Act
-    await articlesPage.addArticleButtomLogged.click();
-    const addArticleView = new AddArticleView(page);
-
-    await expect.soft(addArticleView.header).toBeVisible();
-    const articleData = randomNewArticle();
     await addArticleView.createArticle(articleData);
 
     // Assert
-    const articlePage = new ArticlePage(page);
     await expect.soft(articlePage.articleTittle).toHaveText(articleData.title);
-    await expect.soft(articlePage.articleBody).toHaveText(articleData.body, {useInnerText:true});
+    await expect
+      .soft(articlePage.articleBody)
+      .toHaveText(articleData.body, { useInnerText: true });
+  });
+
+  test('create new articles without title @GAD_R04_01', async () => {
+    // Arrange
+
+    const expectedErrorMessage = 'Article was not created';
+    articleData.title = '';
+
+    //Act
+    await addArticleView.createArticle(articleData);
+
+    // Assert
+    await expect
+      .soft(addArticleView.alertPopup)
+      .toHaveText(expectedErrorMessage);
+  });
+
+  test('create new articles without body @GAD_R04_01', async () => {
+    // Arrange
+
+    const expectedErrorMessage = 'Article was not created';
+    articleData.body = '';
+
+    //Act
+    await addArticleView.createArticle(articleData);
+
+    // Assert
+    await expect
+      .soft(addArticleView.alertPopup)
+      .toHaveText(expectedErrorMessage);
   });
 });
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,31 +1,39 @@
 import { randomUserData } from '../src/factories/user.factory';
+import { RegisterUser } from '../src/models/user.model';
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
+  let registerPage: RegisterPage;
+  let registerUserData: RegisterUser;
+
+  test.beforeEach(async ({ page }) => {
+    registerPage = new RegisterPage(page);
+    registerUserData = randomUserData();
+    await registerPage.goto();
+  });
+
   test('register with correct data and login @GAD_R03_01 @GAD_R03_02 @GAD_R03_03', async ({
     page,
   }) => {
     // Arrange
-    const registerUserData = randomUserData();
+    const expectedAlertPopupText = 'User created';
 
-    // Act
-    const registerPage = new RegisterPage(page);
-    await registerPage.goto();
+    const loginPage = new LoginPage(page);
+    const welcomePage = new WelcomePage(page);
+
     await registerPage.register(registerUserData);
 
     // Assert
-    const expectedAlertPopupText = 'User created';
     expect(registerPage.alertPopUp).toHaveText(expectedAlertPopupText);
-    const loginPage = new LoginPage(page);
+
     await loginPage.waitForPageToLoadUrl();
     const titleLogin = await loginPage.title();
     expect(titleLogin).toContain('Login');
 
-    // Assert
-    const welcomePage = new WelcomePage(page);
+    // Assert test login
     await loginPage.loginNew({
       userEmail: registerUserData.userEmail,
       userPassword: registerUserData.userPassword,
@@ -34,40 +42,31 @@ test.describe('Verify register', () => {
     expect(title).toContain('Welcome');
   });
 
-  test('not register with incorrect data - non valid email @GAD_R03_04', async ({
-    page,
-  }) => {
+  test('not register with incorrect data - non valid email @GAD_R03_04', async () => {
     // Arrange
-    const registerUserData = randomUserData();
+    const expectedErrorText = 'Please provide a valid email address';
     registerUserData.userEmail = '#@1234';
+
     // Act
-    const registerPage = new RegisterPage(page);
-    await registerPage.goto();
     await registerPage.register(registerUserData);
 
     // Assert
-    const expectedErrorText = 'Please provide a valid email address';
     await expect(registerPage.octavalidateEmailError).toHaveText(
       expectedErrorText,
     );
   });
 
-  test('not register with incorrect data - email not provided @GAD_R03_04', async ({
-    page,
-  }) => {
+  test('not register with incorrect data - email not provided @GAD_R03_04', async () => {
     // Arrange
-    const registerUserData = randomUserData();
-    const registerPage = new RegisterPage(page);
+    const expectedErrorText = 'This field is required';
+
+    // Act
     registerPage.userFirstNameInput.fill(registerUserData.userFirstName);
     registerPage.userLastNameInput.fill(registerUserData.userLastName);
     registerPage.userPasswordInput.fill(registerUserData.userPassword);
-    // Act
-
-    await registerPage.goto();
     await registerPage.registerButton.click();
 
     // Assert
-    const expectedErrorText = 'This field is required';
     await expect(registerPage.octavalidateEmailError).toHaveText(
       expectedErrorText,
     );
