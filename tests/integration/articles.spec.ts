f6b37e8b38719ad3ca8db93d4db005373212a2b1
grzesiek <grzegorz.gajownik@paypo.pl>
2024-05-10 21:03:02 +0200

    test: api verification after article creation

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,6 +1,7 @@
 import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomNewArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
+import { waitForResponse } from '@_src/utils/wait.utils';
 
 test.describe('Verify articlesn', () => {
   test('rejest creating article without title @GAD-R04-01 @logged', async ({
@@ -14,9 +15,7 @@ test.describe('Verify articlesn', () => {
     const expectedResponseCode = 422;
     articleData.title = '';
 
-    const responsePromise = page.waitForResponse('/api/articles', {
-      timeout: RESPONSE_TIMEOUT,
-    });
+    const responsePromise = waitForResponse(page, '/api/articles');
     //Act
     await addArticleView.createArticle(articleData);
     const response = await responsePromise;
@@ -50,13 +49,10 @@ test.describe('Verify articlesn', () => {
     addArticleView,
   }) => {
     // Arrange
-
     const expectedErrorMessage = 'Article was not created';
     const articleData = prepareRandomNewArticle(129);
-
     //Act
     await addArticleView.createArticle(articleData);
-
     // Assert
     await expect
       .soft(addArticleView.alertPopup)
@@ -65,15 +61,49 @@ test.describe('Verify articlesn', () => {
 
   test('create article without title with 128 signs @GAD-R04-02 @logged', async ({
     addArticleView,
+    page,
   }) => {
     // Arrange
 
     const articleData = prepareRandomNewArticle(128);
+    const expectedStatusCode = 201;
 
+    const responsePromise = waitForResponse(page, '/api/articles');
     //Act
     const articlePage = await addArticleView.createArticle(articleData);
-
+    const response = await responsePromise;
     // Assert
+    expect(response.status()).toBe(expectedStatusCode);
+
+    await expect.soft(articlePage.articleTittle).toHaveText(articleData.title);
+  });
+
+  test('should retur created article from API @GAD-R07-04 @logged', async ({
+    addArticleView,
+    page,
+  }) => {
+    // Arrange
+    const articleData = prepareRandomNewArticle();
+    const responsePromise = page.waitForResponse(
+      (response) => {
+        console.log(
+          response.request().method(),
+          response.status(),
+          response.url(),
+        );
+        return (
+          response.url().includes('/api/articles') &&
+          response.request().method() == 'GET'
+        );
+      },
+      { timeout: RESPONSE_TIMEOUT },
+    );
+    //Act
+    const articlePage = await addArticleView.createArticle(articleData);
+    const response = await responsePromise;
+    // Assert
+    expect(response.ok()).toBeTruthy()
+
     await expect.soft(articlePage.articleTittle).toHaveText(articleData.title);
   });
 });
