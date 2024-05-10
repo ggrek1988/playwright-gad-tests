cf7ca698d22b8f8dffb1eba323ba27881543bbc0
grzesiek <grzegorz.gajownik@paypo.pl>
2024-05-10 20:17:43 +0200

    chore: backen verification for test

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,24 +1,31 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomNewArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articlesn', () => {
-
   test('rejest creating article without title @GAD-R04-01 @logged', async ({
     addArticleView,
+    page,
   }) => {
     // Arrange
 
     const expectedErrorMessage = 'Article was not created';
     const articleData = prepareRandomNewArticle();
+    const expectedResponseCode = 422;
     articleData.title = '';
 
+    const responsePromise = page.waitForResponse('/api/articles', {
+      timeout: RESPONSE_TIMEOUT,
+    });
     //Act
     await addArticleView.createArticle(articleData);
+    const response = await responsePromise;
 
     // Assert
     await expect
       .soft(addArticleView.alertPopup)
       .toHaveText(expectedErrorMessage);
+    expect(response.status()).toBe(expectedResponseCode);
   });
 
   test('rejest creating article  without body @GAD-R04-01 @logged', async ({
